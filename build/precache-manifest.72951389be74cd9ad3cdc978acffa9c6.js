7deab275ae0093bbf785e646d8e5d88c552935fb
TornikeJ <jmukhadze.tornike@hotmail.com>
2020-12-13 21:10:28 +0400

    luxestate

---
+++ build/precache-manifest.72951389be74cd9ad3cdc978acffa9c6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd208c6be46be2e538acc19fb238d563",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77afd45f8e2e357dfcaa",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f3269e3a76f8e5e22732",
+    "url": "./static/css/main.53828f68.chunk.css"
+  },
+  {
+    "revision": "77afd45f8e2e357dfcaa",
+    "url": "./static/js/2.76535c7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.76535c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3269e3a76f8e5e22732",
+    "url": "./static/js/main.e2629b25.chunk.js"
+  },
+  {
+    "revision": "cb0b56ff4e3454911956",
+    "url": "./static/js/runtime-main.27a937e2.js"
+  },
+  {
+    "revision": "295ac0c9291f790772b2ae1f11123434",
+    "url": "./static/media/img01.295ac0c9.svg"
+  },
+  {
+    "revision": "cf255c911aa96d3bf9376967d1b20dc1",
+    "url": "./static/media/img02.cf255c91.svg"
+  },
+  {
+    "revision": "8ed4c85b3033de9fa8124123d03c9a6b",
+    "url": "./static/media/img03.8ed4c85b.svg"
+  }
+]);
